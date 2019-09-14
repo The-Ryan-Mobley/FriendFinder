1504ba6936b23351b71f509574007b64a9fde275
Ryan Mobley <ryan.mobley94@gmail.com>
2019-09-13 21:56:36 -0500

    working on apis

--- app/public/javascript/logic.js
+++ app/public/javascript/logic.js
@@ -3,17 +3,31 @@ let questions =['question1','question2','question3','question4',
 $(window).on('load', () => {
     qLoop();
 
-    $('#add-btn').on('click',()=>{
+    $('#add-btn').on('click',(event)=>{
+        
+        //event.target
 
     });
 });
 
 function makeInput(qArr,index){
-    let inp = $(`<div class="form-group"><label for="name">${qArr[index]}</label><input type ="number" class="friendForm" name="survey" min="1" max="5"></div>`);
+    let inp = $(`<div class="form-group"><label for="name">${qArr[index]}</label><input type ="number" class="friendForm" id=${index} name="survey" min="1" max="5"></div>`);
     inp.appendTo('#survey-holder');
 }
 function qLoop(){
     for(let i =0; i <10;i++){
         makeInput(questions,i);
     }
+}
+function sendData(){
+    $.post("/api/friends/" + searchedCharacter, (data)=> {
+
+    });
+}
+function readData(){
+    $.get('./api/friends').then((response)=>{
+        console.log(response);
+
+    });
+
 }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
+
+const friends = require('../data/friends.js')
 module.exports = (app)=>{
     app.get("/api/friends", function (req, res) {
         //sends friend res.JSON
+        return res.json(friends);
+
     });
     app.post("/api/friends", function (req, res) {
-        //sends friend JSON
+        //reads friend JSON
+        // req.body hosts is equal to the JSON post sent from the user take that data and add it to friends array
     });
     
     
